2949cfbc7853de86c43cbcdb931611d9087b35ae
Igor Cuckovic <cuckovic@gmail.com>
2018-04-10 15:33:45 +0200

    reducer

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-function createStore() {
+
+// redux
+function createStore(reducer) {
     // The store should have four parts
     // 1. The state
     // 2. Get the state.
@@ -9,6 +11,7 @@ function createStore() {
     let listeners = []
 
     const getState = () => state
+
     const subscribe = (listener) => {
         listeners.push(listener)
 
@@ -17,13 +20,30 @@ function createStore() {
         }
     }
 
+    const dispatch = (action) => {
+        state = reducer(state, action)
+        listeners.forEach((listener) => listener())
+    }
+
     return {
         getState,
-        subscribe
+        subscribe,
+        dispatch
     }
 }
 
-const store = createStore()
+
+// app code
+
+function todos(state = [], action) {
+    if (action.type === "ADD_TODO") {
+        return state.concat(action.todo)
+    }
+    return state
+}
+
+
+const store = createStore(todos);
 
 store.subscribe(() => {
     console.log("The new state is " + store.getState())
